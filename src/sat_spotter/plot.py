1b7a386bb96d0915362cb86b2d1e114f1937ab1c
Maciej Starosielec <maciej@code-snap.com>
2026-03-11 02:12:42 +0100

    added matplotlib pass visualizations

--- src/sat_spotter/plot.py
+++ src/sat_spotter/plot.py
@@ -1,6 +1,63 @@
-from skyfield.api import EarthSatellite
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from skyfield.api import EarthSatellite, load
 from skyfield.toposlib import GeographicPosition
 
+from sat_spotter.config import CONFIG_PATH
+from sat_spotter.display import group_passes
+from sat_spotter.location import observer
+from sat_spotter.predict import find_passes
+from sat_spotter.tle import fetch_tle, load_satellite, parse_tle
 
-def plot_pass(pass_data, satellite: EarthSatellite, location: GeographicPosition):
-    return
+def plot_passes(passes_data):
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    ax.set_ylim(0, 90)
+    ax.set_yticks([0, 30, 60, 90])
+    ax.set_yticklabels(['90°', '60°', '30°', '0°'])
+
+    for p in passes_data:
+        satellite: EarthSatellite = p['satellite']
+        location: GeographicPosition = p['location']
+        rise_time = p['rise']
+        set_time = p['set']
+        ts = load.timescale()
+        time_array = ts.linspace(rise_time, set_time, 50)
+        difference = satellite - location
+        topocentric = difference.at(time_array)
+        alt, az, _ = topocentric.altaz()
+        azimuths = np.radians(az.degrees)
+        radii = 90 - alt.degrees
+        line, = ax.plot(azimuths, radii)                                                       
+        color = line.get_color()
+        ax.plot(azimuths[0], radii[0], 'o', color=color)   # rise point                      
+        ax.plot(azimuths[-1], radii[-1], 'o', color=color)   # set point
+        ax.annotate('rise', xy=(azimuths[0], radii[0]))
+        ax.annotate('set', xy=(azimuths[-1], radii[-1]))
+        ax.annotate(p['name'], xy=(azimuths[25], radii[25]))
+
+    plt.show()
+
+def run_plot(args):
+    observer_lat: float = args.lat
+    observer_lon: float = args.lon
+    prediction_window: int = args.hours
+    minimum_elevation_filter: int = args.elev
+    satellites = json.load(open(CONFIG_PATH))
+    all_passes = []
+    observer_location = observer(observer_lat, observer_lon)
+    for s in satellites:
+        satellite = load_satellite(parse_tle(fetch_tle(s["norad_id"])))
+        if satellite is None:
+            print(f"Skipping {s['name']}")
+            continue
+        times, _ = find_passes(satellite, observer_location, prediction_window)
+        satellite_pass = group_passes(times, satellite, observer_location, minimum_elevation_filter)
+        all_passes.extend(satellite_pass)
+
+    all_passes.sort(key=lambda p: p['rise'].tt)
+    plot_passes(all_passes)
